ec12993d0899c34feb8ecc901cab70d09beb422f
lisa ballard <lballard.cat@gmail.com>
2013-10-21 22:24:47 -0700

    adds a secrets.py template in case we lose one of the real files

---
+++ secrets_template.py
@@ -0,0 +1,17 @@
+DB_USER = ''
+DB_PASS = ''
+AWS_ACCESS_KEY_ID=''
+AWS_SECRET_ACCESS_KEY=''
+AWS_STORAGE_BUCKET_NAME = ''
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = ''
+
+TAR_FILE_PATH =  ''
+base_volumes_path = '';
+FILE_PATH  = base_volumes_path
+
+DERIVED_PATH  = base_volumes_path + 'derived/'
+IMAGE_PATH = ''
+
+
