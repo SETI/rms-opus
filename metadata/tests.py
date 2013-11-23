4d236538b0d85eb7d4854cdc46aef0a4fd5aa0eb
lisa ballard <lballard.cat@gmail.com>
2013-11-23 00:16:43 -0800

    slight edits to metadata and results tests

--- metadata/tests.py
+++ metadata/tests.py
@@ -1,23 +1,101 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+# metadata tests
 
-Replace these with more appropriate tests for your application.
 """
 
+import sys
+# sys.path.append('/home/lballard/opus/')  #srvr
+sys.path.append('/users/lballard/projects/opus/')
+# from opus import settings
+import settings
+from django.core.management import setup_environ
+setup_environ(settings)
+
+
 from django.test import TestCase
+from django.test.client import Client
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from search.views import *
+from results.views import *
+from django.http import QueryDict
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+cursor = connection.cursor()
 
->>> 1 + 1 == 2
-True
-"""}
+class metadataTests(TestCase):
+
+    c = Client()
+    param_name = 'obs_general.planet_id'
+    selections = {}
+    selections[param_name] = ['Saturn']
+
+    def teardown(self):
+        cursor = connection.cursor()
+        cursor.execute("delete from user_searches")
+        cursor.execute("ALTER TABLE user_searches AUTO_INCREMENT = 1")
+        cursor.execute("show tables like %s " , ["cache%"])
+        print "running teardown"
+        for row in cursor:
+            q = 'drop table ' + row[0]
+            print q
+            cursor.execute(q)
+
+
+    def test_resultCount(self):
+        response = self.c.get('/opus/result_count/?planet=Saturn')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, '{"result_count": 0}')
+
+        response = self.c.get('/search/result_count/?planet=Jupiter')
+        self.assertEqual(response.content, '{"result_count": 2000}')
+
+        response = self.c.get('/search/result_count/?planet=Jupiter&target=SKY')
+        self.assertEqual(response.content, '{"result_count": 12}')
+        self.teardown()
+
+        response = self.c.get('/search/result_count/?planet=Jupiter&target=IO,PANDORA')
+        self.assertEqual(response.content, '{"result_count": 38}')
+        self.teardown()
+
+        # some range queries.. single no qtype (defaults any)
+        response = self.c.get('/search/result_count/?ringradius1=60000&ringradius2=80000')
+        self.assertEqual(response.content, '{"result_count": 187}')
+        self.teardown()
+
+        # qtype all
+        response = self.c.get('/search/result_count/?ringradius1=60000&ringradius2=80000&qtype-ringradius=all')
+        self.assertEqual(response.content, '{"result_count": 187}')
+        self.teardown()
+
+        # qtype only
+        response = self.c.get('/search/result_count/?ringradius1=60000&ringradius2=80000&qtype-ringradius=only')
+        self.assertEqual(response.content, '{"result_count": 0}')
+        self.teardown()
+
+        # mult ranges qtype only 1 given as all
+        response = self.c.get('/search/result_count/?ringradius1=60000&ringradius2=80000,120000&qtype-ringradius=all')
+        self.assertEqual(response.content, '{"result_count": 187}')
+        self.teardown()
+
+        # mission and general only
+        response = self.c.get('/search/result_count/?planet=Jupiter&cassiniactivityname=catface')
+        self.assertEqual(response.content, '{"result_count": 1592}')
+        self.teardown()
+
+
+    def test_getValidMults_planet(self):
+        response = self.c.get('opus/mults/ids/?field=planet&planet=Jupiter')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, '{"mults": {"3": 2000}, "field": "planet"}')
+
+    def test_getValidMults_target(self):
+        response = self.c.get('/search/mults/ids/?field=target&planet=Jupiter')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, '{"mults": {"6": 11, "60": 5, "10": 54, "18": 8, "20": 81, "25": 38, "28": 1803}, "field": "target"}')
+        self.teardown()
+
+    def test_getValidMults_by_labels(self):
+        # getting by labels
+        response = self.c.get('/search/mults/labels/?field=target&planet=Jupiter')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, '{"mults": {"EUROPA": 8, "AMALTHEA": 11, "GANYMEDE": 81, "CALLISTO": 54, "JUPITER": 1803, "IO": 38, "THEBE": 5}, "field": "target"}')
 
--- results/tests.py
+++ results/tests.py
@@ -1,23 +1,54 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
+# results tests
 
-Replace these with more appropriate tests for your application.
 """
 
+import sys
+# sys.path.append('/home/lballard/opus/')  #srvr
+sys.path.append('/users/lballard/projects/opus/')
+# from opus import settings
+import settings
+from django.core.management import setup_environ
+setup_environ(settings)
+
+
 from django.test import TestCase
+from django.test.client import Client
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from search.views import *
+from results.views import *
+from django.http import QueryDict
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+cursor = connection.cursor()
 
->>> 1 + 1 == 2
-True
-"""}
+class metadataTests(TestCase):
+
+    c = Client()
+    param_name = 'obs_general.planet_id'
+    selections = {}
+    selections[param_name] = ['Jupiter']
+
+    def teardown(self):
+        cursor = connection.cursor()
+        cursor.execute("delete from user_searches")
+        cursor.execute("ALTER TABLE user_searches AUTO_INCREMENT = 1")
+        cursor.execute("show tables like %s " , ["cache%"])
+        print "running teardown"
+        for row in cursor:
+            q = 'drop table ' + row[0]
+            print q
+            cursor.execute(q)
+
+
+    def test__getResults(self):
+        print 'in getResults test'
+        response = self.c.get('/search/api/data/?planet=Saturn')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.content), 104098)
+
+
+    def test__getImages(self):
+        response = self.c.get('/search/results/images/?planet=Jupiter')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.content), 42292)
 
