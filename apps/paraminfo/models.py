726429b1e4fb2e26a3a8184905ae0e6ce5194ef3
lisa ballard <lballard.cat@gmail.com>
2013-11-28 12:45:20 -0800

    adds method to paraminfo model for finding param_name composite

--- apps/paraminfo/models.py
+++ apps/paraminfo/models.py
@@ -1,80 +1,80 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
-from django.core.urlresolvers import reverse    
+from django.core.urlresolvers import reverse
 
 
 class Group(models.Model):
-    """  
+    """
     Param Groups
-    
-    the search interface has tons of params that can be searched, 
+
+    the search interface has tons of params that can be searched,
     so we divide them into groups, and divide each group into categories
-    
+
     """
     name = models.CharField(max_length=150, blank=True)
     display = models.BooleanField(default=True)
     disp_order = models.IntegerField(blank=True, null=True)
-    alert = models.TextField(blank=True, null=True)  
+    alert = models.TextField(blank=True, null=True)
 
     class Meta:
         db_table = u'groups'
         ordering = ('disp_order',)
 
     def __unicode__(self):
-        return self.name        
+        return self.name
 
 
 
-class Category(models.Model):     
+class Category(models.Model):
     """
     Param Categories
-    
-    the search interface has tons of params that can be searched, 
+
+    the search interface has tons of params that can be searched,
     so we divide them into groups, and divide each group into categories
-    
+
     """
     name = models.CharField(max_length=150, blank=True)
     label = models.CharField(max_length=108, blank=True)
     display = models.BooleanField(default=True)
     disp_order = models.IntegerField(blank=True, null=True)
-    alert = models.TextField(blank=True, null=True)  
+    alert = models.TextField(blank=True, null=True)
     group = models.ForeignKey(Group, blank=True, null = True)
     # group = models.ForeignKey(Groups, related_name="groups", blank=True, null = True)
-    
+
     class Meta:
-        db_table = u'categories' 
+        db_table = u'categories'
         ordering = ('disp_order',)
 
     def __unicode__(self):
         return self.name
-        
-                                
+
+
 RANK_CHOICES = (('0','Advanced'),('1','Basic'))
-      
-class ParamInfo(models.Model):   
-    """  
+
+class ParamInfo(models.Model):
+    """
     This model describes every searchable param in the database (aka fields in the Observations model)
     each has attributes like display, display order, query type, slug, etc..
-     
+
     A word about the mission and instrument fields: these are very important fields because
-    they describe parameters are grouped in the data query string. 
-    
-    We allow for searching accross different missions and instrument (ie: show me all 
-    Observations of the A ring where Voyager ISS was using the red filter and Cassini ISS 
-    was using a violet filter). This wouldn't work in a traditional relational database 
-    (you can't have an Observation that is *both* taken using the Cassini ISS and Voyager ISS) 
-    
-    To acheive this we tag each parameter with 'mission' or 'instrument' if the field is 
+    they describe parameters are grouped in the data query string.
+
+    We allow for searching accross different missions and instrument (ie: show me all
+    Observations of the A ring where Voyager ISS was using the red filter and Cassini ISS
+    was using a violet filter). This wouldn't work in a traditional relational database
+    (you can't have an Observation that is *both* taken using the Cassini ISS and Voyager ISS)
+
+    To acheive this we tag each parameter with 'mission' or 'instrument' if the field is
     specific to one of those datasets, and the query builder >>> insert model here <<< groups
     them appropriately to get all the data
-    
+
     following our example above, the query issued would look like this:
-    
-    select observations.name from observations where 
+
+    select observations.name from observations where
     (planet='Saturn' and target='A ring' and instrument = 'VGISS' and filter = "red")
     union
     (planet='Saturn' and target='A ring' and instrument = 'COISS' and filter = "violet")
-     
+
     """
     category = models.ForeignKey(Category, blank=True, null = True)
     name = models.CharField(max_length=87)
@@ -102,22 +102,25 @@ class ParamInfo(models.Model):
     dict_more_info_name = models.CharField(max_length=255, blank=True, null = True)
     search_form = models.CharField(max_length=63, blank=True, null = True)
     mission = models.CharField(max_length=15, blank=True, null = True)
-    instrument = models.CharField(max_length=15, blank=True, null = True)  
-    sub_heading = models.CharField(max_length=150, blank=True, null = True)  
+    instrument = models.CharField(max_length=15, blank=True, null = True)
+    sub_heading = models.CharField(max_length=150, blank=True, null = True)
     # related_table_name = models.CharField(max_length=42, blank=True, null = True)
     # related_table_field_name = models.CharField(max_length=25, blank=True, null = True)
-    
-    
+
+
 #    group = models.ForeignKey("Groups", blank=True, null = True)
-                     
+
     class Meta:
-        db_table = u'param_info'    
+        db_table = u'param_info'
         ordering = ('disp_order',)
 
     def __unicode__(self):
-        return u"%s" % self.name       
-    
-    # http://djangosnippets.org/snippets/2057/                           
+        return u"%s" % self.name
+
+    def param_name(self):
+        return self.category_name + '.' + self.name
+
+    # http://djangosnippets.org/snippets/2057/
     def save(self, *args, **kwargs):
         model = self.__class__
         # model.objects.update(group)
@@ -132,6 +135,5 @@ class ParamInfo(models.Model):
                 self.disp_order = 0
 
         return super(ParamInfo, self).save(*args, **kwargs)
-                                                               
 
-    
+
