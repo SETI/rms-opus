2e473347779d24742543ea9c70080de390eeb20f
lisa ballard <lballard.cat@gmail.com>
2013-11-28 00:30:22 -0800

    adds view in paraminfo to narrow the table based on whether we are doing surface geo query or not

--- apps/paraminfo/views.py
+++ apps/paraminfo/views.py
@@ -1,6 +1,7 @@
 from paraminfo.models import *
 
-def getParamInfoObjectFromSlug(request_get):
+def narrowParamInfoByRequest(request_get):
+
     param_info = ParamInfo.objects
     if 'surface_target' in request_get:
         param_info = param_info.filter(category_name__contains=request_get['surface_target'])
