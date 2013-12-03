1a9d5bb579e602a3cb1b67001e9b188943ca2da3
lisa ballard <lballard.cat@gmail.com>
2013-12-02 21:30:17 -0800

    formatting

--- urls.py
+++ urls.py
@@ -19,50 +19,40 @@ from paraminfo.models import *
 
 # results - getting data
 
-base_urlpatterns = patterns(  # all files one ring_obs_id
-                         # all files from search
-    'results.views',
+base_urlpatterns = patterns( 'results.views',
     (r'^api/data.(json|zip|html|csv)$', 'getData'),
     (r'^api/detail/(?P<ring_obs_id>\w+).(?P<fmt>[json|zip|html|csv]+)$', 'getDetail'),
-    (r'^api/images/(thumb|small|med|full).(json|zip|html|csv)$',
-     'getImages'),
+    (r'^api/images/(thumb|small|med|full).(json|zip|html|csv)$','getImages'),
     (r'^api/image/(?P<size>[thumb|small|med|full]+)/(?P<ring_obs_id>\w+).(?P<fmt>[json|zip|html|csv]+)$', 'getImage'),
-    (r'^api/files/(?P<ring_obs_id>\w+).(?P<fmt>[json|zip|html|csv]+)$',
-     'getFiles'),
+    (r'^api/files/(?P<ring_obs_id>\w+).(?P<fmt>[json|zip|html|csv]+)$','getFiles'),
     (r'^api/files.(?P<fmt>[json|zip|html|csv]+)$', 'getFiles'),
 )
 
-# metadata - getting information about a data
 
-base_urlpatterns += patterns(  # returns mult values+counts
-                          # returns mult values+counts
-                          # indie field info
-                          # list of all params
-    'metadata.views',
-    (r'^api/meta/result_count.(?P<fmt>[json|zip|html|csv]+)$',
-     'getResultCount'),
+# metadata - getting information about a data
+base_urlpatterns += patterns('metadata.views',
+    (r'^api/meta/result_count.(?P<fmt>[json|zip|html|csv]+)$', 'getResultCount'),
     (r'^api/meta/mults/(?P<slug>[-\sa-zA-Z]+).(?P<fmt>[json|zip|html|csv]+)$', 'getValidMults'),
     (r'^api/meta/range/endpoints/(?P<slug>[-\sa-zA-Z1-9]+).(?P<fmt>[json|zip|html|csv]+)$', 'getRangeEndpoints'),
-    (r'^api/fields/(?P<field>\w+).(?P<fmt>[json|zip|html|csv]+)$',
-     'getFields'),
+    (r'^api/fields/(?P<field>\w+).(?P<fmt>[json|zip|html|csv]+)$','getFields'),
     (r'^api/fields.(?P<fmt>[json|zip|html|csv]+)$', 'getFields'),
-    (r'^api/category/(?P<category>\w+).(?P<fmt>[json|zip|html|csv]+)$',
-     'getCats'),
+    (r'^api/category/(?P<category>\w+).(?P<fmt>[json|zip|html|csv]+)$','getCats'),
     (r'^api/categories.(?P<fmt>[json|zip|html|csv]+)$', 'getCats'),
+    # (r'^api/meta/histogram/(?P<fmt>[labels|ids]+)/.(?P<fmt>[json|zip|html|csv]+)$', 'getValidMults'),   # returns mult values+counts
 )
 
-    #   (r'^api/meta/histogram/(?P<fmt>[labels|ids]+)/.(?P<fmt>[json|zip|html|csv]+)$', 'getValidMults'),   # returns mult values+counts
 
 # making downloads
 base_urlpatterns += patterns('downloads.views',
-                        (r'^zip/(?P<ring_obs_ids>\w+).(?P<fmt>[json]+)$', 'create_download'),
-                        (r'^collections/download/(?P<collection_name>[default]+).zip$', 'create_download'),
-                        (r'^collections/download/info/$',
-                        'get_download_info'))
+    (r'^zip/(?P<ring_obs_ids>\w+).(?P<fmt>[json]+)$', 'create_download'),
+    (r'^collections/download/(?P<collection_name>[default]+).zip$', 'create_download'),
+    (r'^collections/download/info/$',
+    'get_download_info'))
+
 
 # UI resources - the homepage
-base_urlpatterns += patterns(  # (r'^$', direct_to_template, {'template': 'maintenance.html'}),
-    'ui.views',
+base_urlpatterns += patterns('ui.views',
+    # (r'^$', direct_to_template, {'template': 'maintenance.html'}),
     (r'^$', 'mainSite'),
     (r'^table_headers.html$', 'getDataTable'),
     (r'^menu.html$', 'getMenuLabels'),
@@ -74,41 +64,31 @@ base_urlpatterns += patterns(  # (r'^$', direct_to_template, {'template': 'maint
 )
 
 # guide - app runs guide to  api
-
 base_urlpatterns += patterns('guide.views',
-                        (r'^api/$', 'guide'),
-                        (r'^api/guide.html$', 'guide'))  # api help pages
+    (r'^api/$', 'guide'),
+    (r'^api/guide.html$', 'guide'))  # api help pages
 
 # user_collections - all your carts are belong to us
-
-base_urlpatterns += patterns(
-    'user_collections.views',
+base_urlpatterns += patterns('user_collections.views',
     (r'^collections/(?P<collection>[default]+)/(?P<action>[add|remove|addrange|removerange]+).json$', 'edit_collection'),
-    (r'^collections/(?P<collection_name>[default]+)/view.html$',
-     'view_collection'),
-    (r'^collections/(?P<collection>[default]+)/status.json$',
-     'collection_status'),
+    (r'^collections/(?P<collection_name>[default]+)/view.html$','view_collection'),
+    (r'^collections/(?P<collection>[default]+)/status.json$','collection_status'),
     (r'^collections/reset.html$', 'reset_sess'),
     (r'^collections/(?P<collection>[default]+)/(?P<action>[addrange|removerange]+)/test.html$', 'edit_collection_range'),
 )
 
 # mobile - mobile volumes browser
+base_urlpatterns += patterns('mobile.views',
+    (r'^mobile.html$', 'menus'),
+    (r'^mobile/gallery/(?P<volume_id>\w+)/(?P<page>\w+).json$', 'gallery'),
+    (r'^mobile/image/(?P<ring_obs_id>\w+).html$', 'image'),
+    (r'^mobile/detail/(?P<ring_obs_id>\w+).html$', 'detail'))  # api help pages
 
-base_urlpatterns += patterns('mobile.views', (r'^mobile.html$', 'menus'),
-                        (r'^mobile/gallery/(?P<volume_id>\w+)/(?P<page>\w+).json$', 'gallery'),
-                        (r'^mobile/image/(?P<ring_obs_id>\w+).html$',
-                        'image'),
-                        (r'^mobile/detail/(?P<ring_obs_id>\w+).html$',
-                        'detail'))  # api help pages
 
 # django admin
-
 base_urlpatterns += patterns('', (r'^admin/doc/',
                         include('django.contrib.admindocs.urls')),
-                        (r'^admin/', include(admin.site.urls)))  # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-
-                                                                 # to INSTALLED_APPS to enable admin documentation:
-                                                                 # Uncomment the next line to enable the admin:
+                        (r'^admin/', include(admin.site.urls)))
 
 if settings.DEBUG:
     base_urlpatterns += patterns('django.views.static',
