acead73bb3fdeaa92b67d206c613f8319141cd49
lisa ballard <lballard.cat@gmail.com>
2014-05-21 22:00:44 -0700

    implements colorbox slideshow

--- static_media/js/browse.js
+++ static_media/js/browse.js
@@ -647,6 +647,32 @@ var o_browse = {
                }
                appendBrowsePage();
 
+                var $overflow = '';
+                var colorbox_params = {
+                    rel: 'colorbox',
+                    reposition:true,
+                    scalePhotos:true,
+                    scrolling:false,
+                    previous:'<i class="ace-icon fa fa-arrow-left"></i>',
+                    next:'<i class="ace-icon fa fa-arrow-right"></i>',
+                    close:'&times;',
+                    current:'{current} of {total}',
+                    maxWidth:'100%',
+                    maxHeight:'100%',
+                    onOpen:function(){
+                        $overflow = document.body.style.overflow;
+                        document.body.style.overflow = 'hidden';
+                    },
+                    onClosed:function(){
+                        document.body.style.overflow = $overflow;
+                    },
+                    onComplete:function(){
+                        $.colorbox.resize();
+                    }
+                };
+
+    $('.ace-thumbnails [data-rel="colorbox"]').colorbox(colorbox_params);
+
                /* we are removing this footer, scrolling is always auto
                // if user is in auto mode remove the classes from the footer that make it appear clickable
                if (opus.browse_auto == 'checked' && !opus.browse_footer_style_disabled) {
