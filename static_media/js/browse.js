511c6dba16d0c09a955dbf4848add2729dbb2838
lisa ballard <lballard.cat@gmail.com>
2014-05-24 23:36:26 -0700

    fixes all broken icons (which were all of them)

--- static_media/js/browse.js
+++ static_media/js/browse.js
@@ -647,29 +647,32 @@ var o_browse = {
                }
                appendBrowsePage();
 
-                var $overflow = '';
-                var colorbox_params = {
-                    rel: 'colorbox',
-                    reposition:true,
-                    scalePhotos:true,
-                    scrolling:false,
-                    previous:'<i class="ace-icon fa fa-arrow-left"></i>',
-                    next:'<i class="ace-icon fa fa-arrow-right"></i>',
-                    close:'&times;',
-                    current:'{current} of {total}',
-                    maxWidth:'100%',
-                    maxHeight:'100%',
-                    onOpen:function(){
-                        $overflow = document.body.style.overflow;
-                        document.body.style.overflow = 'hidden';
-                    },
-                    onClosed:function(){
-                        document.body.style.overflow = $overflow;
-                    },
-                    onComplete:function(){
-                        $.colorbox.resize();
-                    }
-                };
+            var $overflow = '';
+            var colorbox_params = {
+                rel: 'colorbox',
+
+                className:"gallery_overlay_bg",
+
+                reposition:false,
+                scalePhotos:true,
+                scrolling:false,
+                previous: '<i class="ace-icon fa fa-arrow-left"></i>',
+                next: '<i class="ace-icon fa fa-arrow-right"></i>',
+                close:'&times;',
+                current:'{current} of {total}',
+                maxWidth:'100%',
+                maxHeight:'100%',
+                onOpen:function(){
+                    $overflow = document.body.style.overflow;
+                    document.body.style.overflow = 'hidden';
+                },
+                onClosed:function(){
+                    document.body.style.overflow = $overflow;
+                },
+                onComplete:function(){
+                    $.colorbox.resize();
+                }
+            };
 
     $('.ace-thumbnails [data-rel="colorbox"]').colorbox(colorbox_params);
 
