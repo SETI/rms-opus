3240b08ac92a704d849e4ecdeede14dc705559d8
lisa ballard <lballard.cat@gmail.com>
2014-09-04 19:42:45 -0700

    -mmakes cache_clear.py also clear memcache and removes a hard coded base path

--- deploy/cache_clear.py
+++ deploy/cache_clear.py
@@ -1,6 +1,6 @@
 import sys
-base_path = '/home/django/djcode/opus/'
-sys.path.append(base_path)
+from settings import PROJECT_ROOT
+sys.path.append(PROJECT_ROOT)
 
 from django.conf import settings
 from settings import CACHES
@@ -9,4 +9,4 @@ settings.configure(CACHES=CACHES) # include any other settings you might need
 
 from django.core.cache import cache
 cache.clear()
-
+cache._cache.flush_all()  # clears memcache hopefully only on this port!
