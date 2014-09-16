6a7f6997eb16f334245ad122c5c40c7476f3a79e
lisa ballard <lballard.cat@gmail.com>
2014-09-16 14:14:28 -0700

    fixes links in api guide

--- apps/guide/views.py
+++ apps/guide/views.py
@@ -1,13 +1,13 @@
 from guide.models import *
-from django.template import RequestContext    
+from django.template import RequestContext
 from django.shortcuts import render_to_response
-from django.http import HttpResponse,Http404  
+from django.http import HttpResponse,Http404
 
-def guide(request):   
-    base_url = 'http://' +request.META['HTTP_HOST'] + '/'  
-    groups = Group.objects.all()     
-    resources = Resource.objects.filter(display=True).select_related().order_by('disp_order')      
-                                                                           
+def guide(request):
+    base_url = 'http://' +request.META['HTTP_HOST'] + '/opus/'
+    groups = Group.objects.all()
+    resources = Resource.objects.filter(display=True).select_related().order_by('disp_order')
+
     return render_to_response('guide.html',locals(), context_instance=RequestContext(request))
 
 
