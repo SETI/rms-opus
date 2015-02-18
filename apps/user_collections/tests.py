adeb1a63c70c312facb174efb22cff48503ce8a9
lisa ballard <lballard.cat@gmail.com>
2015-02-17 16:52:59 -0800

    gets session handling setup properly in user_collections tests

--- apps/user_collections/tests.py
+++ apps/user_collections/tests.py
@@ -2,32 +2,36 @@
 #  user_collections
 
 """
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.test.client import Client
 from django.db.models import get_model
-from  django.utils.importlib import import_module
+from django.contrib.auth.models import AnonymousUser, User
+from django.utils.importlib import import_module
 from search.views import *
 from results.views import *
 from django.http import QueryDict
 
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_COOKIE_NAME = 'opus-test-cookie'
+
 cursor = connection.cursor()
+
+class test_session(dict):
+    session_key = 'test_key'
 
 class user_CollectionsTests(TestCase):
 
     def setUp(self):
         self.client = Client()
-        """
-        set up sessions for anonymous users
-        """
-        engine = import_module(settings.SESSION_ENGINE)
-        settings
-        store = engine.SessionStore()
-        store.save()
-        self.client.cookies[settings.SESSION_COOKIE_NAME] = store.session_key
+        self.factory = RequestFactory()
 
     def test__edit_collection(self):
-        response = self.client.get('/opus/collections/default/add.json?request=1&ringobsid=S_IMG_CO_ISS_1680806160_N', HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        request = self.factory.get('/opus/collections/default/add.json?request=1&ringobsid=S_IMG_CO_ISS_1680806160_N', HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        request.user = AnonymousUser()
+        request.session = test_session()
+        response = edit_collection(request, ring_obs_id = 'S_IMG_CO_ISS_1680806160_N', action = 'add')
         self.assertEqual(response.status_code, 200)
-        print response.content
-        self.assertGreater(len(response.content), 42)
+        expected = '{"count": 1, "request_no": 1, "err": false}'
+        self.assertEqual(expected, response.content)
+
 
