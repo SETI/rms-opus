c26870d8041033775880546a61ccafbf86207095
Lisa Ballard <lballard.cat@gmail.com>
2016-03-24 21:56:11 -0700

    adds script for running selenium tests against production endpoints

---
+++ tests/api_tests.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import json
+import csv
+
+api_endpoint = "http://tools.pds-rings.seti.org/opus/api/meta/result_count.json?"
+filename = 'result_counts.txt'
+
+
+with open(filename, 'rb') as csvfile:
+
+    filereader = csv.reader(csvfile)
+    for row in filereader:
+
+        q_str, expected = row
+
+        url_hash = q_str.split('#/')[1].strip()
+        api_url = api_endpoint + url_hash
+
+        driver = webdriver.Firefox()
+        driver.get(api_url)
+        soup = BeautifulSoup(driver.page_source)
+        dict_from_json = json.loads(soup.find("body").text)
+
+        print "checking.. " + api_url
+        print "expecting " + expected
+        print dict_from_json['data'][0]['result_count']
+        assert dict_from_json['data'][0]['result_count'] >= int(expected)
+        driver.close()
+
