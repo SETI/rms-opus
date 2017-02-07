b6064a2341b6eb6b3d7f3f2b21d614bf444c807a
Lisa Ballard <lballard.cat@gmail.com>
2017-02-06 16:22:59 -0800

    fixes session handler for running tests on prod server

--- apps/metrics/views.py
+++ apps/metrics/views.py
@@ -1,14 +1,16 @@
 from metrics.models import Metrics
 
 def update_metrics(request):
+
     if not request:
         return  # this is a test running
 
-    if not request.session.get('has_session'):
+    session_id = request.session.session_key
+
+    if not session_id:
         # this user has barely touched the homepage #todo test this better
         return
 
-    session_id = request.session.session_key
 
     ip_address = get_client_ip(request)
     m,v = Metrics.objects.using('metrics').get_or_create(session_id=session_id, ip_address=ip_address)
