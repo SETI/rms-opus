e43316b33194b80d6ea1eab91b01879d52b43784
Lisa Ballard <lballard.cat@gmail.com>
2018-01-24 17:58:07 -0800

    API guide redux, moves content out of db and into yaml file for easier editing

--- apps/guide/README.md
+++ apps/guide/README.md
@@ -1,1 +1,3 @@
 The guide app runs the API guide web pages at opus/api/guide.html
+
+Edit the examples.yaml file to edit or add new API endpoint examples. 
--- apps/guide/views.py
+++ apps/guide/views.py
@@ -1,16 +1,14 @@
-from guide.models import *
+import yaml, os
 from django.shortcuts import render
 from django.http import HttpResponse,Http404
 from metrics.views import update_metrics
 
 def guide(request):
-    update_metrics(request)
+    path = os.path.dirname(os.path.abspath(__file__))
+    with open(path + "/examples.yaml", 'r') as stream:
+        try:
+            guide = yaml.load(stream)
+            return render(request, 'guide.html', locals())
 
-    base_url = 'http://' +request.META['HTTP_HOST'] + '/opus/'
-    groups = Group.objects.all()
-    resources = Resource.objects.filter(display=True).select_related().order_by('disp_order')
-
-    return render(request, 'guide.html', locals())
-
-
-# def update(request)
+        except yaml.YAMLError as exc:
+            print(exc)
