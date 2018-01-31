4b46c60338dfab00711368ac4209d723c39a55a7
Lisa Ballard <lballard.cat@gmail.com>
2018-01-31 14:55:17 -0800

    fab deploy redux so it works more generally for other humans

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,23 +1,51 @@
 # -*- coding: utf-8 -*-
 from fabric.api import run, settings, env, cd, lcd, prompt, local
 from fabric.contrib.console import confirm
-from slackclient import SlackClient
-from secrets import SLACK_CHANNEL, SLACK_TOKEN, REMOTE_USERNAME
 from fabric.network import ssh
+from slackclient import SlackClient
+from secrets import REMOTE_USERNAME, REMOTE_USER_ROOT_PATH, REMOTE_WEB_ROOT_PATH, \
+                    LOCAL_OPUS_PATH, LOCAL_GIT_CLONE_PATH, \
+                    SLACK_CHANNEL, SLACK_TOKEN
 
 """
-this is generally run like:
+    # This is generally run like:
 
-    fab tests_local push deploy cache_reboot tests
+        fab tests_local push deploy cache_reboot tests
+
+    # Background
+
+    This script is for deploying to remote installs of opus.
+    It will prompt you to specify prod/dev server.
+
+    The push directive will clone the repo from your *local* git repo of opus
+    and clone it into a separate directory (to avoid pushing any untracked content)
+    Then it will push that repo to your home directory on the remote server.
+
+    The deploy directive will create a copy of the previously deployed repo into backups/.
+    and then copy the newly pushed github into the web root on that server.
+
+    Run cache_reboot to refresh the web interface on the remote server.
+
+    The tests directive will run the django tests on the remote install.
+
+    # Initial Setup
+
+    ## Edit the secrets.py file
+    cp secrets.template.py secrets.py
+
+    On the server you will need the following directory to exist
+    in your remote home directory:
+
+        backups/
 
 """
-
-local_root_path = '/Users/lballard/'
-remote_root_path = '/home/lballard/'
-
 # ssh.util.log_to_file("paramiko.log", 10)
 
-env.use_ssh_config = True
+prod_deploy_dir_name = 'opus'  # NO trailing slash here.
+                               # this is the name of the opus django directory
+                               # in the dev/production web root
+
+env.use_ssh_config = True  # untrue only on dev if no ssl cert
 
 host = prompt("production or dev server?", default='dev')
 
@@ -28,9 +56,7 @@ elif 'dev' in host:
 else:
     sys.exit('please supply a host "prod" or "dev"')
 
-
-prod_deploy_dir = 'opus'
-static_assets_path = remote_root_path + 'rdsk/www/assets/' # static_media goes here
+static_assets_path = REMOTE_USER_ROOT_PATH + 'rdsk/www/assets/' # static_media goes here
 # git_branch = 'menu_bug'
 git_branch = 'master'
 git_revision = ''  # use this to roll back to a specific commit
@@ -41,7 +67,7 @@ def tests_local():
     """
     runs all unit tests locally
     """
-    with lcd(local_root_path + '/projects/opus/'):
+    with lcd(LOCAL_GIT_CLONE_PATH + '/projects/opus/'):
         local("REUSE_DB=1 python manage.py test apps -x")
 
 
@@ -53,8 +79,8 @@ def push():
     """
 
     # then checkout code from repo in another directory, and transfer that copy to server
-    with lcd(local_root_path):
-        # checks out git repo into local dir local_root_path/opus
+    with lcd(LOCAL_GIT_CLONE_PATH):
+        # checks out git repo into local dir LOCAL_GIT_CLONE_PATH/opus
         # then rsyncs that copy to production
 
         # clean up old deploys
@@ -62,27 +88,22 @@ def push():
 
         # grab the local repo (this is all because couldn't grab remote from server)
         # local('git clone -b ' + git_branch + ' git@bitbucket.org:ringsnode/opus2.git')  # does not
-        local('git clone -b %s file:///%sprojects/opus' % (git_branch, local_root_path))
+        local('git clone -b {} file:///{}'.format(git_branch, LOCAL_OPUS_PATH))
 
-        # the repo gets named opus when you clone it, but you can name
-        # that directory something else if you want, this handles that:
-        if prod_deploy_dir != 'opus':
-            local('rm -rf %s' % prod_deploy_dir)
-            local('mv opus %s' % prod_deploy_dir) # rename it down here before rsyncing it
-
-    with lcd('%sopus/' % local_root_path):
+    with lcd('%sopus/' % LOCAL_GIT_CLONE_PATH):
         if git_revision:
             local('git checkout %s' % git_revision)
 
-    with lcd(local_root_path):
+    with lcd(LOCAL_GIT_CLONE_PATH):
         # zip the javascript files,  why it commented out, broken?
         # local('python opus/deploy/deploy.py')
+
         # rsync that code to staging directory on production
-        local('rsync -r -vc -e ssh --exclude .git %s %s@%s:~/.' % (prod_deploy_dir, REMOTE_USERNAME, env.hosts[0]))
+        local('rsync -r -vc -e ssh --exclude .git %s %s@%s:~/.' % (prod_deploy_dir_name, REMOTE_USERNAME, env.hosts[0]))
 
     # move static_media into the right place
-    with cd(remote_root_path):
-        run("sudo cp -r %s/static_media %s." % (prod_deploy_dir, static_assets_path))
+    with cd(REMOTE_USER_ROOT_PATH):
+        run("sudo cp -r %s/static_media %s." % (prod_deploy_dir_name, static_assets_path))
 
 
 def deploy():
@@ -90,25 +111,25 @@ def deploy():
     take a backup of the currently deployed source on the server
     then move staged copy to production location
     """
-    with cd(remote_root_path):
+    with cd(REMOTE_USER_ROOT_PATH):
         # first take a backup:
-        run('sudo rsync -r -vc --exclude logs /home/django/djcode/' + prod_deploy_dir + ' backups/.')
+        run('sudo rsync -r -vc --exclude logs {}{} backups/.'.format(REMOTE_WEB_ROOT_PATH,prod_deploy_dir_name))
 
         # copy the new code to production directory
         # exclude certain directories from deploying to production website
         exclude_str = "--exclude logs --exclude .git  --exclude import --exclude deploy"
-        run('sudo rsync -r -vc {} {} /home/django/djcode/.'.format(exclude_str, prod_deploy_dir))
+        run('sudo rsync -r -vc {} {} /home/django/djcode/.'.format(exclude_str, prod_deploy_dir_name))
 
 
 def cache_reboot():
     """
     refreshes python code wsgi, django caches, browser last modified, and memcached.
     """
-    with cd(remote_root_path):
+    with cd(REMOTE_USER_ROOT_PATH):
 
         # refresh the *.wsgi files (not sure which ones are actually doing job)
-        run('sudo touch /home/django/djcode/' + prod_deploy_dir + '/*.wsgi')
-        run('sudo touch /home/django/djcode/' + prod_deploy_dir + '/apache/*.wsgi')
+        run('sudo touch {}{}/*.wsgi'.format(REMOTE_WEB_ROOT_PATH,prod_deploy_dir_name))
+        run('sudo touch /home/django/djcode/{}{}/apache/*.wsgi'.format(REMOTE_WEB_ROOT_PATH,prod_deploy_dir_name))
 
         # tells browsers something has changed
         run('sudo python /home/django/djcode/opus/apps/tools/reset_deploy_datetime.py')
@@ -121,14 +142,14 @@ def cache_reboot():
         run('/usr/bin/memcached -d -l 127.0.0.1 -m 1024 -p %s' % memcached_port)
 
         # reset django cache
-        run('sudo python /home/django/djcode/' + prod_deploy_dir + '/deploy/cache_clear.py')
+        run('sudo python /home/django/djcode/{}{}/deploy/cache_clear.py'.format(REMOTE_WEB_ROOT_PATH, prod_deploy_dir_name))
 
 
 def tests():
     """ runs full unit tests on production """
     # run all tests on production and
     # if that flies notifies slack channel that opus has been deployed
-    with cd('/home/django/djcode/%s/' % prod_deploy_dir):
+    with cd('{}{}'.format(REMOTE_WEB_ROOT_PATH, prod_deploy_dir_name)):
         # this only runs a few app's test suites because the others have problems
         # where every Client() request.get returns a 404, unless you load it in a browser
         # first, then it runs ok, so something is awry in production testing.. todo
--- deploy/secrets.template.py
+++ deploy/secrets.template.py
@@ -1,2 +1,18 @@
-SLACK_TOKEN = ''
-SLACK_CHANNEL = ''
+# remote server settings
+REMOTE_USERNAME = ''  # your remote user login name
+REMOTE_WEB_ROOT_PATH = ''  # all _PATH vars require trailling slash
+
+REMOTE_ROOT_PATH = ''  # This is your home directory on remote server
+                       # You will end up with a clone copy of opus here.
+
+# local settings
+LOCAL_OPUS_PATH = ''  # your local clone of the opus repo, your working project root
+LOCAL_GIT_CLONE_PATH = ''  # a clone of the opus repo will be created here
+                           # make sure it's not the same directory as your
+                           # project working directory, becuase this
+                           # directory gets added/deleted by this script (see push())
+
+SLACK_TOKEN = ''   # Empty is OK:
+SLACK_CHANNEL = '' # this is for a final step in the deploy, a  slack bot that
+                   # posts a msg to Rings Node slack channel
+                   # If this fails it does not effect the deploy
