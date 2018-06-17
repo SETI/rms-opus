ffbe6bf5fb0397ceb952afb415f35f18f9f35e31
Robert French <rfrench@rfrench.org>
2018-06-17 11:59:05 -0700

    Clean up URL regex to allow underscores in slugs

--- apps/metadata/urls.py
+++ apps/metadata/urls.py
@@ -9,8 +9,8 @@ from metadata.views import (
 # metadata - getting information about a data
 urlpatterns = [
     url(r'^api/meta/result_count.(?P<fmt>[json|zip|html|csv]+)$', getResultCount),
-    url(r'^api/meta/mults/(?P<slug>[-\sa-zA-Z0-9]+).(?P<fmt>[json|zip|html|csv]+)$', getValidMults),
-    url(r'^api/meta/range/endpoints/(?P<slug>[-\sa-zA-Z0-9]+).(?P<fmt>[json|zip|html|csv]+)$', getRangeEndpoints),
+    url(r'^api/meta/mults/(?P<slug>[-\w]+).(?P<fmt>[json|zip|html|csv]+)$', getValidMults),
+    url(r'^api/meta/range/endpoints/(?P<slug>[-\w]+).(?P<fmt>[json|zip|html|csv]+)$', getRangeEndpoints),
     url(r'^api/fields/(?P<field>\w+).(?P<fmt>[json|zip|html|csv]+)$', getFields),
     url(r'^api/fields.(?P<fmt>[json|zip|html|csv]+)$', getFields),
 ]
