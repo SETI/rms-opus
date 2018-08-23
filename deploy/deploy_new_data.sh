d690f4d16021de5d0360eb66da351296dc525b01
Robert French <rfrench@rfrench.org>
2018-08-22 17:13:18 -0700

    Update deploy script

--- deploy/deploy_new_data.sh
+++ deploy/deploy_new_data.sh
@@ -1,4 +1,5 @@
 #/bin/sh
+(cd ../opus/application; python manage.py migrate)
 (cd ../opus/application; yes yes | python manage.py collectstatic)
 (cd ../opus/application; python clear_django_cache.py)
 systemctl restart memcached
