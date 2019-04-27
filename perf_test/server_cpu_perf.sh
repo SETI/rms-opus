6056f94bdaef23f67b41debcb1576cbd7f89624f
Robert French <rfrench@seti.org>
2019-04-27 10:49:30 -0700

    Update server perf

--- perf_test/server_cpu_perf.sh
+++ perf_test/server_cpu_perf.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
-echo *** INT64 ***
-.
-1
-d
-a
-echo *** SINGLE CPU - INT64 ***
+echo "*** SINGLE CPU - INT64 ***"
 stress-ng --cpu 1 --cpu-method int64 -t 10s --metrics-brief
-echo *** ALL CPUs - INT64 ***
+echo "*** ALL CPUs - INT64 ***"
 stress-ng --cpu 0 --cpu-method int64 -t 10s --metrics-brief
-echo *** SINGLE CPU - MATRIX MULT ***
-stress-ng --cpu 1 --cpu-method matrix -t 10s --metrics-brief
-echo *** ALL CPUs - MATRIX MULT ***
-stress-ng --cpu 0 --cpu-method matrix -t 10s --metrics-brief
-echo *** SINGLE CPU - FFT ***
+echo "*** SINGLE CPU - FFT ***"
 stress-ng --cpu 1 --cpu-method fft -t 10s --metrics-brief
-echo *** ALL CPUs - FFT ***
+echo "*** ALL CPUs - FFT ***"
 stress-ng --cpu 0 --cpu-method fft -t 10s --metrics-brief
+echo "*** SINGLE CPU - MATRIX MULT ***"
+stress-ng --matrix 1 -t 10s --metrics-brief
+echo "*** ALL CPUs - MATRIX MULT ***"
+stress-ng --matrix 0 -t 10s --metrics-brief
+
