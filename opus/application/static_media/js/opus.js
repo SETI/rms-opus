15ea678e933cb254023f981e7e39f5bce1d2bd8e
Robert French <rfrench@seti.org>
2019-06-16 11:06:46 -0700

    Update user agent checking

--- opus/application/static_media/js/opus.js
+++ opus/application/static_media/js/opus.js
@@ -91,8 +91,10 @@ var opus = {
     browserSupport: {
         "firefox": 59,
         "chrome": 56,
+        "chrome (ios)": 56,
         "opera": 42,
         "safari": 10.1,
+        "based on applewebkit": 537, // Based on Chrome 56
         "width": 600,
         "height": 350
     },
@@ -829,48 +831,115 @@ var opus = {
         opus.triggerNavbarClick();
     },
 
-    isBrowserSupported: function() {
+    checkBrowserSupported: function() {
         /**
          * Check supported browser versions and display a modal to
          * inform the user if that version is not supprted.
          */
         let browserName, browserVersion, matchObj;
         let userAgent = navigator.userAgent;
-        let updateString = "Please use a supported browser for a better experience.";
+        let updateString = "";
 
-        if (userAgent.indexOf("Firefox") > -1) {
-            // Example output:
+        // Good information on how to parse User Agent strings:
+        // https://deviceatlas.com/blog/user-agent-parsing-how-it-works-and-how-it-can-be-used
+        // https://deviceatlas.com/blog/user-agent-string-analysis
+        // NOTE:
+        //   Mozilla/5.0 means "Mozilla compatible"
+        //   Safari/xxx means "Safari compatible"
+
+        if (userAgent.indexOf("Firefox/") > -1) {
+            // ==== FIREFOX ====
+            // ** Mac:
             // Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0)
             // Gecko/20100101 Firefox/66.0
+            // ** Windows:
+            // Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0)
+            // Gecko/20100101 Firefox/67.0
+            // ** Linux:
+            // Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0)
+            // Gecko/20100101 Firefox/66.0
+            // ** Android:
+            // Mozilla/5.0 (Android 7.0; Mobile; rv:54.0)
+            // Gecko/54.0 Firefox/54.0
             matchObj = userAgent.match(/Firefox\/(\d+.\d+)/);
             browserName = "Firefox";
             browserVersion = matchObj[1];
         } else if (userAgent.indexOf("OPR") > -1) {
-            // userAgent example output:
+            // ==== OPERA ====
+            // ** Mac:
             // Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36
             // (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36 OPR/60.0.3255.95
+            // ** Mobile:
+            // Mozilla/5.0 (Linux; Android 7.0; SM-A310F Build/NRD90M) AppleWebKit/537.36
+            // (KHTML, like Gecko) Chrome/55.0.2883.91 Mobile Safari/537.36 OPR/42.7.2246.114996
+            // Opera/9.80 (Android 4.1.2; Linux; Opera Mobi/ADR-1305251841) Presto/2.11.355 Version/12.10
             matchObj = userAgent.match(/OPR\/(\d+.\d+)/);
             browserName = "Opera";
             browserVersion = matchObj[1];
         } else if (userAgent.indexOf("Chrome") > -1 && userAgent.indexOf("Edge") === -1 &&
                    userAgent.indexOf("Edg") === -1) {
-            // userAgent example output:
+            // ==== CHROME ====
+            // ** Mac:
             // Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36
             // (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36
+            // ** Windows:
+            // Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36
+            // (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36
+            // ** Linux:
+            // Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36
+            // (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36
+            // ** Android:
+            // Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36
+            // (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36
+            // ** Chrome OS:
+            // Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36
+            // (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36
             matchObj = userAgent.match(/Chrome\/(\d+.\d+)/);
             browserName = "Chrome";
             browserVersion = matchObj[1];
+        } else if (userAgent.indexOf("CriOS") > -1) {
+            // ==== CHROME (iOS) ====
+            // ** iOS:
+            // Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15
+            // (KHTML, like Gecko) CriOS/69.0.3497.105 Mobile/15E148 Safari/605.1
+            matchObj = userAgent.match(/CriOS\/(\d+.\d+)/);
+            browserName = "Chrome (iOS)";
+            browserVersion = matchObj[1];
         } else if (userAgent.indexOf("Version") > -1) {
-            // userAgent example output:
+            // ==== SAFARI ====
+            // ** Mac:
             // Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15
             // (KHTML, like Gecko) Version/12.1 Safari/605.1.15
+            // ** iOS:
+            // Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30
+            // (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1
             matchObj = userAgent.match(/Version\/(\d+.\d+)/);
             browserName = "Safari";
+            browserVersion = matchObj[1];
+        } else if (userAgent.indexOf("AppleWebKit") > -1 && userAgent.indexOf("Edge") === -1 &&
+                   userAgent.indexOf("Edg") === -1) {
+            // ==== Other AppleWebKit-based Browser ====
+            // I don't know why, but Edge doesn't work even though it claims to have the right version of AWK
+            // ** Firefox (iOS)
+            // Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4
+            // (KHTML, like Gecko) FxiOS/7.5b3349 Mobile/14F89 Safari/603.2.4
+            // ** Facebook Messenger:
+            // Mozilla/5.0 (iPad; CPU OS 9_3_5 like Mac OS X) AppleWebKit/601.1.46
+            // (KHTML, like Gecko) Mobile/13G36
+            // [FBAN/MessengerForiOS;FBAV/100.1.0.36.68;FBBV/46154306;FBRV/0;FBDV/iPad4,2;
+            //  FBMD/iPad;FBSN/iPhone OS;FBSV/9.3.5;FBSS/2;FBCR/Viettel;FBID/tablet;FBLC/en_US;FBOP/5]
+            // ** Facebook Mobile App:
+            // Mozilla/5.0 (iPhone; CPU iPhone OS 11_4_1 like Mac OS X) AppleWebKit/605.1.15
+            // (KHTML, like Gecko) Mobile/15G77
+            // [FBAN/FBIOS;FBAV/183.0.0.41.81;FBBV/119182652;FBDV/iPhone8,1;
+            //  FBMD/iPhone;FBSN/iOS;FBSV/11.4.1;FBSS/2;FBCR/Oi;FBID/phone;FBLC/pt_BR;FBOP/5;FBRV/0]
+            matchObj = userAgent.match(/AppleWebKit\/(\d+.\d+)/);
+            browserName = "based on AppleWebKit";
             browserVersion = matchObj[1];
         } else {
             browserName = `unsupported - ${userAgent}`;
             browserVersion = "";
-            updateString = "";
+            updateString = "Please use a supported browser for a better experience.";
         }
 
         let browser = `${browserName} ${browserVersion}</br></br>OPUS may not work properly on your browser.`;
@@ -879,7 +948,8 @@ var opus = {
                         Firefox (${opus.browserSupport.firefox}+),
                         Chrome (${opus.browserSupport.chrome}+),
                         Safari (${opus.browserSupport.safari}+),
-                        and Opera (${opus.browserSupport.opera}+).
+                        Opera (${opus.browserSupport.opera}+),
+                        and other AppleWebKit-based browsers (${opus.browserSupport["based on applewebkit"]})+.
                         ${updateString}`);
         $("#op-browser-version-msg .modal-body").html(modalMsg);
         browserName = browserName.toLowerCase();
@@ -929,11 +999,10 @@ var opus = {
 }; // end opus namespace
 
 $(document).ready(function() {
-    if (opus.isBrowserSupported()) {
-        opus.checkVisitedCookie();
-        opus.checkBrowserSize();
-        // Call normalized url api first
-        // Rest of initialization prcoess will be performed afterwards
-        opus.normalizedURLAPICall();
-    }
+    opus.checkBrowserSupported();
+    opus.checkVisitedCookie();
+    opus.checkBrowserSize();
+    // Call normalized url api first
+    // Rest of initialization process will be performed afterwards
+    opus.normalizedURLAPICall();
 });
