03439f3df14ec511fa67859587d6618eaeacdc03
Robert French <rfrench@seti.org>
2019-07-02 21:41:37 -0700

    Change search tooltip text. Improve accuracy of URL encoding.

--- opus/application/apps/results/templatetags/encode_value.py
+++ opus/application/apps/results/templatetags/encode_value.py
@@ -1,9 +1,10 @@
 from django import template
 
+import urllib
+
 register = template.Library()
 
 @register.filter(name='encode_value')
 def encode_value(value):
     "Encode a search value for use in an OPUS URL."
-    value = str(value).replace(' ', '%20').replace('+', '%2B')
-    return value
+    return urllib.parse.quote_plus(value, "-_.!~*'()")
