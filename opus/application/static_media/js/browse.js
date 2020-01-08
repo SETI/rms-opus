a0a7d652a8ae1e94d233f1df6a1ad77d0648b835
Debby Stopp <stoppmeister@gmail.com>
2020-01-07 23:35:23 -0800

    -initial changes to support a mini-thumbnail in the table view for both browse and cart tabs.

--- opus/application/static_media/js/browse.js
+++ opus/application/static_media/js/browse.js
@@ -349,6 +349,20 @@ var o_browse = {
             o_browse.confirmationBeforeAddAll();
         });
 
+        // Click to zoom/shrink all mini-thumbs in the table view
+        // NOTE: at this time we are not keeping the preference of zoom vs shrink
+        $(".op-data-table-view").on("click", ".op-mini-thumbnail-zoom, .op-mini-thumbnail-shrink", function(e) {
+            let tab = opus.getViewTab();
+            let viewNamespace = opus.getViewNamespace();
+            if ($(this).hasClass("op-mini-thumbnail-zoom")) {
+                $(".op-mini-thumbnail-zoom").removeClass("op-mini-thumbnail-zoom").addClass("op-mini-thumbnail-shrink");
+                $(this).children().prop('title', 'Click to shrink all mini-thumbnails in the table');
+            } else {
+                $(".op-mini-thumbnail-shrink").removeClass("op-mini-thumbnail-shrink").addClass("op-mini-thumbnail-zoom");
+                $(this).children().prop('title', 'Click to enlarge all mini-thumbnails in the table');
+            }
+        });
+
         // browse sort order - remove sort slug
         $(".sort-contents").on("click", "li .remove-sort", function() {
             o_browse.showPageLoaderSpinner();
@@ -1451,6 +1465,10 @@ var o_browse = {
                 let checkbox = `<input type="checkbox" name="${opusId}" value="${opusId}" class="multichoice"${checked}/>`;
                 let minimenu = `<a href="#" data-icon="menu" title="More options"><i class="fas fa-bars fa-xs"></i></a>`;
                 let row = `<td class="op-table-tools"><div class="op-tools mx-0 form-group" title="Click to ${buttonInfo[tab].title.toLowerCase()}\r\nShift+click to start/end range" data-id="${opusId}">${checkbox} ${minimenu}</div></td>`;
+
+                let miniThumbnail = `<img src="${images.thumb.url}" alt="${images.thumb.alt_text}" title="${mainTitle}">`;
+                row += `<td class="op-mini-thumbnail op-mini-thumbnail-zoom"><div>${miniThumbnail}</div></td>`;
+
                 let tr = `<tr data-id="${opusId}" ${recycled} data-target="#galleryView" data-obs="${item.obs_num}" title="${mainTitle}">`;
                 $.each(item.metadata, function(index, cell) {
                     row += `<td>${cell}</td>`;
@@ -1514,11 +1532,15 @@ var o_browse = {
                          "class='op-table-header-addall btn btn-link'>" +
                          "<i class='fas fa-cart-plus' data-action='addall'" +
                          " title='Add All Results to Cart'></i></button>";
-        let tableHeaderFirstCol = "<th scope='col' class='sticky-header op-table-first-col'>" +
-                                  "<div>" + addallIcon + "</div></th>";
+        let thumbnailCol = "<button type='button'" +
+                           "class='op-mini-thumbnail-zoom btn btn-link'>" +
+                           "<i class='far fa-images' title='Click to enlarge all mini-thumbnail in the table'></i>" +
+                           "</button>";
+        let tableHeaderFirstCol = `<th scope='col' class='sticky-header op-noresize'><div>${addallIcon}</div></th>`;
+        let tableHeaderThumbnailCol = `<th scope='col' class='sticky-header op-noresize'><div>${thumbnailCol}</div></th>`;
         $(`${tab} .op-data-table-view thead`).append("<tr></tr>");
         $(`${tab} .op-data-table-view thead tr`).append(tableHeaderFirstCol);
-        // $(`${tab} .op-data-table-view thead tr`).append("<th scope='col' class='sticky-header'></th>");
+        $(`${tab} .op-data-table-view thead tr`).append(tableHeaderThumbnailCol);
 
         $.each(columns, function(index, header) {
             let slug = slugs[index];
