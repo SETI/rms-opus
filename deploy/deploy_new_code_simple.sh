c25bc29f295c1c7467e6f1219f51357a8fa67267
Robert French <rfrench@seti.org>
2020-02-27 17:05:07 -0800

    Minor changes to deploy script; add opus.wsgi_template

--- deploy/deploy_new_code_simple.sh
+++ deploy/deploy_new_code_simple.sh
@@ -1,7 +1,7 @@
 #/bin/sh
-systemctl stop apache2
+sudo systemctl stop apache2
 (cd ../opus/application; yes yes | python manage.py collectstatic)
 (cd ../opus/application; python clear_django_cache.py)
 (cd ../opus/import; python main_opus_import.py --create-param-info --create-partables --create-table-names --create-grouping-target-name --import-dict)
-systemctl restart memcached
-systemctl start apache2
+sudo systemctl restart memcached
+sudo systemctl start apache2
