f85328bdcdfe705688b902bba7059f04980aa4be
fyellin <fy@fyellin.com>
2020-03-18 17:10:20 -0700

    Fix bug in error_analyzer.  Start on --ignore-errors-file (#1012)
    * Minor cleanup
    
    * Let error-analysis handle cronjob args, also
    Cronjob args can take more than one file pattern
    Remove unnecessary assertion.
    
    * Remove print statement that's not supposed to be there.
    
    * For error analyzer, only take one day for cronjob
    
    * Use US/Pacific for --cronjob when no date given.
    
    * Fix bugs
    add --ignore-errors-file

--- log_analyzer/cronjob_utils.py
+++ log_analyzer/cronjob_utils.py
@@ -50,19 +50,19 @@ def __parse_cronjob_date_arg(args: Namespace) -> datetime.datetime:
         today = datetime.datetime.now(tz=DEFAULT_TIMEZONE).replace(hour=0, minute=0, second=0, microsecond=0)
         return today
     # if the argument is -<number>, then it means that many days ago
-    match = re.match(r'-(\d+)', cronjob_date)
+    match = re.fullmatch(r'-(\d+)', cronjob_date)
     if match:
         today = datetime.datetime.now(tz=DEFAULT_TIMEZONE).replace(hour=0, minute=0, second=0, microsecond=0)
         return today - datetime.timedelta(days=int(match.group(1)))
     # if the argument is dddd-dd, then it is a year and month, and indicates the last day of that month
-    match = re.match(r'(\d\d\d\d)-(\d\d)', cronjob_date)
+    match = re.fullmatch(r'(\d\d\d\d)-(\d\d)', cronjob_date)
     if match:
         year_month = datetime.datetime(
             tzinfo=DEFAULT_TIMEZONE, year=int(match.group(1)), month=int(match.group(2)), day=1)
         sometime_following_month = year_month + datetime.timedelta(days=31)
         return sometime_following_month - datetime.timedelta(days=sometime_following_month.day)
     # if the argument is dddd-dd-dd, then treat it as year-month-day
-    match = re.match(r'(\d\d\d\d)-(\d\d)-(\d\d)', cronjob_date)
+    match = re.fullmatch(r'(\d\d\d\d)-(\d\d)-(\d\d)', cronjob_date)
     if match:
         return datetime.datetime(
             tzinfo=DEFAULT_TIMEZONE, year=int(match.group(1)), month=int(match.group(2)), day=int(match.group(3)))
