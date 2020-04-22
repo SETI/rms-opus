4ae5ba91e1c516105b69a6f0404e0f5148d06c70
Frank Yellin <fy@fyellin.com>
2020-04-22 13:15:20 -0700

    Fix flags

--- log_analyzer/opus/configuration_flags.py
+++ log_analyzer/opus/configuration_flags.py
@@ -1,4 +1,5 @@
 from enum import Flag, auto
+from typing import Sequence
 
 
 class InfoFlags(Flag):
@@ -10,7 +11,7 @@ class InfoFlags(Flag):
     VIEWED_DETAIL_TAB = auto()
     VIEWED_SLIDE_SHOW = auto()
     VIEWED_SELECT_METADATA = auto()
-    CHANGED_SELECTED_METADATA = auto() #
+    CHANGED_SELECTED_METADATA = auto()
     DOWNLOADED_CSV_FILE_FOR_ALL_RESULTS = auto()
     DOWNLOADED_CSV_FILE_FOR_ONE_OBSERVATION = auto()
     DOWNLOADED_ZIP_FILE_FOR_ONE_OBSERVATION = auto()
@@ -20,6 +21,17 @@ class InfoFlags(Flag):
     VIEWED_HELP_FILE_AS_PDF = auto()
     HAS_OBSOLETE_SLUG = auto()
 
-    def get_fancy_name(self):
+    def get_fancy_name(self) -> str:
         name = self.name.lower().replace("_", " ").replace("csv", "CSV").replace("zip", "ZIP").replace('pdf', "PDF")
         return name[0].upper() + name[1:]
+
+    def as_list(self) -> Sequence['InfoFlags']:
+        return [flag for flag in InfoFlags if flag in self]
+
+
+class ActionFlags(Flag):
+    HAS_SEARCH = auto()
+    FETCHED_GALLERY = auto()
+    HAS_METADATA = auto()
+    HAS_DOWNLOAD = auto()
+    HAS_OBSOLETE_SLUG = auto()
