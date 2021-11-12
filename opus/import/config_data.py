b2082e9465fe3e44491c38392fda4244092183e6
Yu-Jen Chang <juzen2003@gmail.com>
2021-11-12 09:23:35 -0800

    Add datatime tests for _is_voyager_at_north* functions for VGPPS & VGUVS. - In populate_obs_instrument_VGPPS_prof.py (line 378-418), add datetime   tests for for _is_voyager_at_north* functions. - In populate_obs_instrument_VGUVS_prof.py (line 380-420), add datetime   tests for for _is_voyager_at_north* functions.

--- opus/import/config_data.py
+++ opus/import/config_data.py
@@ -1148,7 +1148,23 @@ DESIRED_VOLUMES_IN_VOLSET = {
     'COCIRS_0xxx': r'(COCIRS_040[2-9]|COCIRS_041\d|COCIRS_0[5-9]\d{2})$',
 }
 
-# Note: threshold value 1986-01-24T17:10:13.320 is obtained and determined by
-# observing the results from OPUS. Voyager is at north when start time is before
+# Note: threshold values are obtained and determined by observing the results
+# from OPUS. Voyager location:
+# North
+# 1980-11-12T05:15:45.520
+# South
+# 1980-11-13T04:19:30.640
+# North
+# 1981-08-26T04:18:21.080
+# South
+# 1985-11-06T17:22:30.040
+# North
 # 1986-01-24T17:10:13.320
-THRESHOLD_START_TIME_VG_AT_NORTH = '1986-01-24T17:10:13.320'
+# South
+THRESHOLD_START_TIME_VG_AT_NORTH = [
+    '1980-11-12T05:15:45.520',
+    '1980-11-13T04:19:30.640',
+    '1981-08-26T04:16:45.080',
+    '1985-11-06T17:22:30.040',
+    '1986-01-24T17:10:13.320'
+]
