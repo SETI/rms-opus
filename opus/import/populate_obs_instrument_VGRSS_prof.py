f1d36e63ed3e99f658068206b77349035d43e1e0
Yu-Jen Chang <juzen2003@gmail.com>
2021-11-12 14:54:07 -0800

    Update to fetch wavelength data from profile index for VG_2803 because values in supplemental index files are not correct.

--- opus/import/populate_obs_instrument_VGRSS_prof.py
+++ opus/import/populate_obs_instrument_VGRSS_prof.py
@@ -126,13 +126,13 @@ def populate_obs_type_image_VGRSS_greater_pixel_size_PROF(**kwargs):
 
 def populate_obs_wavelength_VGRSS_wavelength1_PROF(**kwargs):
     metadata = kwargs['metadata']
-    index_row = metadata['supp_index_row']
+    index_row = metadata['index_row']
     wl1 = index_row['MINIMUM_WAVELENGTH']
     return wl1
 
 def populate_obs_wavelength_VGRSS_wavelength2_PROF(**kwargs):
     metadata = kwargs['metadata']
-    index_row = metadata['supp_index_row']
+    index_row = metadata['index_row']
     wl2 = index_row['MAXIMUM_WAVELENGTH']
     return wl2
 
@@ -219,11 +219,7 @@ def populate_obs_occultation_VGRSS_optical_depth_max_PROF(**kwargs):
     return None
 
 def populate_obs_occultation_VGRSS_temporal_sampling_PROF(**kwargs):
-    metadata = kwargs['metadata']
-    index_row = metadata['supp_index_row']
-    tmp_sampling_interval = index_row['TEMPORAL_SAMPLING_INTERVAL']
-
-    return tmp_sampling_interval
+    return None
 
 def populate_obs_occultation_VGRSS_quality_score_PROF(**kwargs):
     return 'GOOD'
