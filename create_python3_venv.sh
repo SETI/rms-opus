77d648963fedcb2015f1acb503dbc5ab137d9259
Robert French <rfrench@rfrench.org>
2022-01-24 11:51:04 -0800

    Add .gitattributes and move pull request template.

--- create_python3_venv.sh
+++
@@ -1,6 +0,0 @@
-# source create_python3_venv.sh
-python3.6 -m venv --without-pip p3venv
-source p3venv/bin/activate
-curl https://bootstrap.pypa.io/get-pip.py | python
-deactivate
-source p3venv/bin/activate
